c0517d19c081c8cc573f679024e8f2910ebef6ff
guojingzhan <guojingzhan@jd.com>
2025-10-07 23:14:08 +0800

    51. N 皇后

--- src/main/java/com/jan/title100/title60/Title51.java
+++ src/main/java/com/jan/title100/title60/Title51.java
@@ -1,5 +1,9 @@
 package com.jan.title100.title60;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -24,11 +28,43 @@ import java.util.List;
  */
 public class Title51 {
     public static void main(String[] args) {
-
+        System.out.println(JSON.toJSONString(solveNQueens(4)));
+        System.out.println(JSON.toJSONString(solveNQueens(2)));
+        System.out.println(JSON.toJSONString(solveNQueens(1)));
     }
 
-    public List<List<String>> solveNQueens(int n) {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> ans = new ArrayList<>();
+        int[] queens = new int[n];
+        boolean[] col = new boolean[n];
+        boolean[] diag1 = new boolean[2 * n - 1];
+        boolean[] diag2 = new boolean[2 * n - 1];
+        dfs(0, queens, col, diag1, diag2, ans);
+        return ans;
+    }
 
-        return null;
+    private static void dfs(int r, int[] queens, boolean[] col, boolean[] diag1, boolean[] diag2, List<List<String>> ans) {
+        int n = col.length;
+        if (r == n) {
+            List<String> board = new ArrayList<>(n);
+            for(int c : queens) {
+                char[] row = new char[n];
+                Arrays.fill(row, '.');
+                row[c] = 'Q';
+                board.add(new String(row));
+            }
+            ans.add(board);
+            return;
+        }
+        // 在 (r,c) 放皇后
+        for (int c = 0; c < n; c++) {
+            int rc = r - c + n - 1;
+            if(!col[c] && !diag1[r + c] && !diag2[rc]) {    // 判断能否放皇后
+                queens[c] = r;                              // 直接覆盖，无需恢复现场
+                col[c] = diag1[r + c] = diag2[rc] = true;   // 皇后占用了 c 列和两条斜线
+                dfs(r + 1, queens, col, diag1, diag2, ans);
+                col[c] = diag1[r + c] = diag2[rc] = false;  // 恢复现场
+            }
+        }
     }
 }
